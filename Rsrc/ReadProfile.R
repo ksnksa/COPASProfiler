1f169cede20120cd58f50f43b28effe1dbbdcc33
Khalifa Alnaim <khalifaalnaim@Khalifas-MacBook-Air.local>
2020-11-02 17:05:07 +0300

    Added SamplePlot and modified readprofile function!

--- Rsrc/ReadProfile.R
+++ Rsrc/ReadProfile.R
@@ -1,4 +1,5 @@
 ReadProfile <- function(filename,ID){ #I wasn't sure what the max.size and min.size stood for, will the user input the length of the signals that they want?
+  library(reshape)
   ch0 <- read.delim((paste(getwd(),'/',filename,'_profil_ch0_prf.txt',sep='')), header=TRUE)
   ch1 <- read.delim((paste(getwd(),'/',filename,'_profil_ch1_prf.txt',sep='')), header=TRUE)
   ch2 <- read.delim((paste(getwd(),'/',filename,'_profil_ch2_prf.txt',sep='')), header=TRUE)
@@ -12,5 +13,6 @@ ReadProfile <- function(filename,ID){ #I wasn't sure what the max.size and min.s
   datafr <- data.frame('EXT' = as.numeric(ch0[1:maxrow,ID]),'Green' = as.numeric(ch1[1:maxrow,ID]),'Yellow' = as.numeric(ch2[1:maxrow,ID]),'Red' = as.numeric(ch3[1:maxrow,ID]))
   datafr$Length <- 1:maxrow 
   newdatafr <- melt(datafr, id.vars = "Length", measure.vars =c('EXT','Green','Yellow','Red'))
+  colnames(newdatafr) = c('Length','variable','Amplitude')
   return(newdatafr)
    }
---
+++ analysis/SamplePlot.R
@@ -0,0 +1,6 @@
+library(ggplot2)
+source(paste(getwd(),'/Rsrc/ReadProfile.R',sep=''))
+filedirectory <- '/Data/N2/n2'
+ID <- 1998 #My birth year 
+profile <- ReadProfile(filedirectory,ID)
+ggplot(profile, aes(x=Length, y=Amplitude, group=variable,color=variable)) + geom_line()+ scale_colour_manual(values = c('black','green','yellow','red')) + labs(color='Channel') + ggtitle(paste('Lucky worm #',ID,sep=''))
