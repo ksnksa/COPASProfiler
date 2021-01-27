306d76941d11ec081dba6efe38b17ee9b661982b
Khalifa Alnaim <khalifaalnaim@Khalifas-MacBook-Air.local>
2021-01-27 14:48:56 +0300

    Made some changes to the plotcluster functions so it returns a list of plots

--- Rsrc/PlotClusters.R
+++ Rsrc/PlotClusters.R
@@ -1,12 +1,17 @@
+require(ggplot2)
+require(reshape)
+require(dplyr)
 PlotClusters <- function(Channel,ClusterTable) {
+  PlotList <- list()
+  counter <- 1
   if(missing(Channel)){ 
     stop('Channel dataframe was not provided')
   } else if(missing(ClusterTable)) {
     stop('Cluster list was not provided')
-  } else if(!list(ClusterTable)) {
-    stop('Cluster list provided is not a list')
-  } else if(!frame(Channel)) {
-    stop('Channel dataframe provided is not a dataframe')
+  } else if(!is.list(ClusterTable)) {
+    stop('Cluster list provided is not a list')}
+  else if(!is.matrix(Channel)) {
+   stop('Channel dataframe provided is not a dataframe')
   }
   Stage <- c('L1','L23','L4','Adult')
   for (i in 1:4) {
@@ -40,17 +45,23 @@ PlotClusters <- function(Channel,ClusterTable) {
     test$Length <- 1:maxrow
     if(nrow(cluster_IDs) > 50){maxL = 50}else{maxL = nrow(cluster_IDs)}
     newdatafr <- melt(test, id.vars = "Length", measure.vars = c(cluster_IDs[1:maxL,])) 
-    
+    stdmean <- 0
+    for (j in nrow(cluster_IDs)) {
+      stdmean <- stdmean + sd(Channel[cluster_IDs[j,1],])
+    }
+    stdmean <- stdmean/nrow(cluster_IDs)
+  
     
     p = ggplot(newdatafr, aes(Length, value,col = variable)) +
       geom_line(color="grey") +
       geom_line(aes(x=Length, y=Mean),clustermean, color = 'red') + #the mean will be the red line 
       theme_minimal() +
-      ylab("amp") + xlab("") 
-    
-    
-    ggsave(paste("Cluster_",cl_id,Stage[i],".pdf",sep=""),p)
+      ylab("amp") + xlab("") +
+     ggtitle(paste('Stage: ',Stage[i],' Cluster# ',cl_id,' With ',nrow(cluster_IDs),' lucky worms',' std=',stdmean,sep=''))
+    PlotList[[counter]] <- p
+    counter <- counter +1 
     }
+    
   }
-  
+  return(PlotList)
 }
