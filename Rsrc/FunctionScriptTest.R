a46e0aa8c481fc0d53bef4055125a9abb600575c
Khalifa Alnaim <khalifaalnaim@Khalifas-MacBook-Air.local>
2021-01-27 14:49:59 +0300

    Slightlty made the script to test the functions better

--- Rsrc/FunctionScriptTest.R
+++ Rsrc/FunctionScriptTest.R
@@ -8,14 +8,14 @@ Ch2D <- (paste(getwd(),'/data/N2','/','n2','_profil_ch2_prf.txt',sep=''))
 Ch3D <- (paste(getwd(),'/data/N2','/','n2','_profil_ch3_prf.txt',sep=''))
 source((paste(getwd(),'/Rsrc','/','ReadChannel.R',sep='')))
 channellist <- ReadChannel(Ch0D,Ch1D,Ch2D,Ch3D)
-ch0 <- channellist[[1]]
-ch1 <- channellist[[2]]
-ch2 <- channellist[[3]]
-ch3 <- channellist[[4]]
+
+
+
 source((paste(getwd(),'/Rsrc','/','FilterChannel.R',sep='')))
-Index <- FilterChannel(ch0,64000,40,500)
-ch1 <- ch1[-c(Index),]
+Index <- FilterChannel(channellist[[1]],64000,40,500)
+channellist[[2]] <- channellist[[2]][-c(Index),]
 source((paste(getwd(),'/Rsrc','/','ClusterChannel.R',sep='')))
-clusterlist <- ClusterChannel(ch1,10)
+clusterlist <- ClusterChannel(channellist[[2]],10)
+ch1 <- channellist[[2]]
 source((paste(getwd(),'/Rsrc','/','PlotClusters.R',sep='')))
-PlotClusters(ch1,clusterlist)
+p <- PlotClusters(ch1,clusterlist)
