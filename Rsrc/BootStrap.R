c89dff12717b9928dbe345322e81c28dd9d6ccca
Khalifa Alnaim <khalifaalnaim@khalifas-air.kaust.edu.sa>
2021-02-21 12:35:06 +0300

    Added for loop to change number of samples

--- Rsrc/BootStrap.R
+++ Rsrc/BootStrap.R
@@ -34,50 +34,52 @@ GoodIndex <- matrix(GoodIndex, ncol = ncol(GoodIndex), dimnames = NULL)
 #For loop that randomly picks a specific number of good worms (from the training set) to try and predict the remaining worms
 AdultGoodIndex <- matrix(GoodIndex[1,which(GoodIndex[1,] %in% rownames(Adult))]) 
 BadWorms <- Adult[-which(rownames(Adult) %in% AdultGoodIndex),]
-PredictionMean <- 0 
 Accuracy <- data.frame()
-for (o in 1:100) {
-  #Change the size to change how many are picked
-  SampleSize <- 25
-  RandomIndex <- sample(c(1:length(AdultGoodIndex)), size=SampleSize, replace = FALSE)
-  RandomIndex <- sort(RandomIndex)
-  ComplementVec <- 1:length(AdultGoodIndex)
-  ComplementVec <- ComplementVec[-RandomIndex]
-  RandomIndex <- AdultGoodIndex[RandomIndex]
-  PredIndex <- AdultGoodIndex[ComplementVec]
-  IndexMax <- max( as.numeric(sub('X','',RandomIndex)))
-  PredBadWormsIndex <- sample(c(1:nrow(BadWorms)),size = (5*length(ComplementVec)), replace = FALSE)
-  PredBadWormsIndex <- sort(PredBadWormsIndex)
-  PredictionSet <- as.data.frame(rbind(BadWorms[PredBadWormsIndex,],Adult[as.numeric(sub('X','',which(rownames(Adult) %in% PredIndex))),]))
-  #Matrix with randomly picked worms 
-  TrainingSet <- as.data.frame(rbind(BadWorms[-PredBadWormsIndex,]),Adult[as.numeric(sub('X','',which(rownames(Adult) %in% RandomIndex))),])
-  #training_Adult <- Adult[1:(which(as.numeric(sub('X','',rownames(Adult))) >= IndexMax)[1]),] #Creating a dataset that has all the worms up to the highest index of the good worms 
-  #All the good worms 
-  #FullTrainingSet <- Adult[1:(which(as.numeric(sub('X','',rownames(Adult))) >= max(as.numeric(sub('X','',GoodIndex))))[1]),] #Creating a prediction table with all the worms 
-  #Creating the truth table 
-  #training_Adult <- as.data.frame(training_Adult)
-  TrainingSet$Factor <- as.numeric(1) #creating a true false table
-  TrainingSet$Factor[(nrow(TrainingSet)-(SampleSize - 1)):nrow(TrainingSet)] <- as.numeric(2) #changing it to true for the good worms (true being 2 and false is 1)
-  x <- lapply(TrainingSet[,1:1124],as.numeric) #it's up to 1124 (becease we added one column factor, so we take it off by doing this to make training set same size as prediction set?)
-  m <- as.data.frame(x)
-  y <- TrainingSet$Factor #make y variable(dependent)
-  model <- svm(m, y,type='C-classification',
-               scale=TRUE,
-               kernel="polynomial")
-  pred <- predict(model, PredictionSet)
-  #Sanity check, are all the training data in the predection?
+NumberOfRuns <- 3
+for (l in 1:(length(AdultGoodIndex)-1)) {
+  
+  for (o in 1:NumberOfRuns) {
+    #Change the size to change how many are picked
+    SampleSize <- l
+    RandomIndex <- sample(c(1:length(AdultGoodIndex)), size=SampleSize, replace = FALSE)
+    RandomIndex <- sort(RandomIndex)
+    ComplementVec <- 1:length(AdultGoodIndex)
+    ComplementVec <- ComplementVec[-RandomIndex]
+    RandomIndex <- AdultGoodIndex[RandomIndex]
+    PredIndex <- AdultGoodIndex[ComplementVec]
+    IndexMax <- max( as.numeric(sub('X','',RandomIndex)))
+    PredBadWormsIndex <- sample(c(1:nrow(BadWorms)),size = (2*length(ComplementVec)), replace = FALSE)
+    PredBadWormsIndex <- sort(PredBadWormsIndex)
+    PredictionSet <- as.data.frame(rbind(BadWorms[PredBadWormsIndex,],Adult[as.numeric(sub('X','',which(rownames(Adult) %in% PredIndex))),]))
+    #Matrix with randomly picked worms 
+    TrainingSet <- as.data.frame(rbind(BadWorms[-PredBadWormsIndex,]),Adult[as.numeric(sub('X','',which(rownames(Adult) %in% RandomIndex))),])
+    #training_Adult <- Adult[1:(which(as.numeric(sub('X','',rownames(Adult))) >= IndexMax)[1]),] #Creating a dataset that has all the worms up to the highest index of the good worms 
+    #All the good worms 
+    #FullTrainingSet <- Adult[1:(which(as.numeric(sub('X','',rownames(Adult))) >= max(as.numeric(sub('X','',GoodIndex))))[1]),] #Creating a prediction table with all the worms 
+    #Creating the truth table 
+    #training_Adult <- as.data.frame(training_Adult)
+    TrainingSet$Factor <- as.numeric(1) #creating a true false table
+    TrainingSet$Factor[(nrow(TrainingSet)-(SampleSize - 1)):nrow(TrainingSet)] <- as.numeric(2) #changing it to true for the good worms (true being 2 and false is 1)
+    x <- lapply(TrainingSet[,1:1124],as.numeric) #it's up to 1124 (becease we added one column factor, so we take it off by doing this to make training set same size as prediction set?)
+    m <- as.data.frame(x)
+    y <- TrainingSet$Factor #make y variable(dependent)
+    model <- svm(m, y,type='C-classification',
+                 scale=TRUE,
+                 kernel="polynomial")
+    pred <- predict(model, PredictionSet)
+    #Sanity check, are all the training data in the predection?
     #PredLength <- length(which(pred == 2)) 
     #CorrectPredictionPercent <- 1 - (length(ComplementVec) - PredLength)/length(ComplementVec)
     #PredictionMean <- (PredictionMean + CorrectPredictionPercent) /2
-  Positive <- rownames(PredictionSet[which(pred==2),])
-  TP <- sum((Positive %in% GoodIDs), na.rm = TRUE)
-  Negative <- rownames(PredictionSet[which(pred==1),])
-  TN <- length(Negative %in% GoodIDs) - sum((Negative %in% GoodIDs), na.rm = TRUE)
-  Accuracy[1,o] <- (TP + TN) / (length(Positive) + length(Negative))
-  Accuracy[2,o] <- o
+    Positive <- rownames(PredictionSet[which(pred==2),])
+    TP <- sum((Positive %in% GoodIDs), na.rm = TRUE)
+    Negative <- rownames(PredictionSet[which(pred==1),])
+    TN <- length(Negative %in% GoodIDs) - sum((Negative %in% GoodIDs), na.rm = TRUE)
+    Accuracy[l,o] <- (TP + TN) / (length(Positive) + length(Negative))
+  }
 }
 y<-as.numeric(Accuracy[1,])
-x<-as.numeric(Accuracy[2,])
+x<-1:length(y)
 df<-data.frame(x=x,y=y)
 df_molten=melt(df,id.vars="x")
 ggplot(df_molten) + geom_line(aes(x=x,y=y,color=variable))
