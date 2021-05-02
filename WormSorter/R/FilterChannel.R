6e743f828746c589f457e3dd68bb5332fb9a91be
Khalifa Alnaim <khalifaalnaim@khalifas-air.kaust.edu.sa>
2021-05-02 13:32:00 +0300

    Added new the WormSorter package

---
+++ WormSorter/R/FilterChannel.R
@@ -0,0 +1,46 @@
+#' Filter channel data
+#'
+#' This function takes in the channel data and filters it according to the input parameters. The parameters are:
+#' Max Time of flight, Minimum length, and maximum length.
+#'
+#' @param ch Channel data
+#' @param MaxTOF Maximum time of flight
+#' @param MinLength Minimum length
+#' @param MaxLength Maximum length
+#'
+#' @return Returns the index number of the filtered objects
+#' @export
+FilterChannel <- function(ch,MaxTOF, MinLength,MaxLength) {
+  #Filters the inputted channel by Max TOF or/and by min length and max length
+  if(missing(MaxTOF) & missing(MinLength) & missing(MaxLength)){
+    stop('Please input the MaxTOF or/and the Minimum and Maximum Worm Length ')
+  } else if(!missing(MinLength) & missing(MaxLength)) {
+    stop('Minimum Worm Length was provided without the Max length')
+  } else if(!missing(MaxLength) & missing(MinLength)) {
+    stop('Maximum Worm Length was provided without the Min length')
+  } else if(missing(ch)) {
+    stop('Channel 0 was not provided')
+  }
+  if (!missing(MaxTOF) & !missing(MinLength)) {
+    WormNumber <- which(ch > MaxTOF, arr.ind = TRUE)
+    WormNumber <- WormNumber[,1]
+    for (x in 1:length(ch[,1])) {
+      if (which.min(ch[x,]) > MaxLength | which.min(ch[x,]) < MinLength) {
+        WormNumber <- c(WormNumber,x)
+      }
+    }
+    WormNumber <- sort(unique(WormNumber))
+  } else if (!missing(MinLength)) {
+    for (x in 1:length(ch[,1])) {
+      if (which.min(ch[x,]) > MaxLength | which.min(ch[x,]) < MinLength) {
+        WormNumber <- c(WormNumber,x)
+      }
+    }
+    WormNumber <- sort(unique(WormNumber))
+  } else if (!missing(MaxTOF)) {
+    WormNumber <- unique(which(ch > MaxTOF, arr.ind = TRUE))
+    WormNumber <- sort(unique(WormNumber[,1]))
+  }
+  return(WormNumber)
+
+}
