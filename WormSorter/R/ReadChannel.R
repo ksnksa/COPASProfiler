05a9bd98e3b2fc635f656cb5675d2b631e3f2cf3
ksnksa <73164879+ksnksa@users.noreply.github.com>
2021-06-10 10:02:50 +0300

    Added Rdata, testing script and more

--- WormSorter/R/ReadChannel.R
+++ WormSorter/R/ReadChannel.R
@@ -14,13 +14,14 @@ ReadChannel <- function(Ch0Directory,Ch1Directory,Ch2Directory,Ch3Directory) {
   if(missing(Ch0Directory)){
     stop('Missing Channel 0 Directory')
   } else if(missing(Ch1Directory)) {
-    stop('Missing Channel 1 Directory')
+    # pending small changes here!!!
+    #
+    #
   } else if(missing(Ch2Directory)) {
     stop('Missing Channel 2 Directory')
   } else if(missing(Ch3Directory)) {
     stop('Missing Channel 3 Directory')
-  }
-  Ch0 <- read.delim(Ch0Directory, header=TRUE)
+  } else { Ch0 <- read.delim(Ch0Directory, header=TRUE)
   Ch1 <- read.delim(Ch1Directory, header=TRUE)
   Ch2 <- read.delim(Ch2Directory, header=TRUE)
   Ch3 <- read.delim(Ch3Directory, header=TRUE)
@@ -30,11 +31,7 @@ ReadChannel <- function(Ch0Directory,Ch1Directory,Ch2Directory,Ch3Directory) {
     stop('Channel 0 and Channel 2 do not have the same dimensions')
   } else if (nrow(Ch0) != nrow(Ch3) | ncol(Ch0) != ncol(Ch3)) {
     stop('Channel 0 and Channel 3 do not have the same dimensions')
-  }
-
-  # sum(Ch0[1,]==Ch1[1,]) == ncol(Ch0)
-
-  if (sum((Ch0[1,1:100] == Ch1[1,1:100])[1,]) == 100) {
+  } else { if (sum((Ch0[1,1:100] == Ch1[1,1:100])[1,]) == 100) {
     stop('Channel 0 and Channel 1 are the same channel')
   } else if (sum((Ch0[1,1:100] == Ch2[1,1:100])[1,]) == 100) {
     stop('Channel 0 and Channel 2 are the same channel')
@@ -49,10 +46,17 @@ ReadChannel <- function(Ch0Directory,Ch1Directory,Ch2Directory,Ch3Directory) {
   }
 
 
-  Ch0 = t(Ch0[,-c(which(is.na(Ch0[1,])))])
-  Ch1 = t(Ch1[,-c(which(is.na(Ch1[1,])))])
-  Ch2 = t(Ch2[,-c(which(is.na(Ch2[1,])))])
-  Ch3 = t(Ch3[,-c(which(is.na(Ch3[1,])))])
+    Ch0 = t(Ch0[,-c(which(is.na(Ch0[1,])))])
+    Ch1 = t(Ch1[,-c(which(is.na(Ch1[1,])))])
+    Ch2 = t(Ch2[,-c(which(is.na(Ch2[1,])))])
+    Ch3 = t(Ch3[,-c(which(is.na(Ch3[1,])))])
+
+
+  }
+
+    }
+
+  # sum(Ch0[1,]==Ch1[1,]) == ncol(Ch0)
 
 
   return(list(Ch0,Ch1,Ch2,Ch3))
