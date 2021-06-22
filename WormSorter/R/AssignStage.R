e8d440080fc935b7370bfe173c773516d025434b
ksnksa <73164879+ksnksa@users.noreply.github.com>
2021-06-22 11:58:09 +0300

    Updated the assign stage function
    Now it matches the thresholds sonia used

--- WormSorter/R/AssignStage.R
+++ WormSorter/R/AssignStage.R
@@ -1,10 +1,10 @@
 #' Assign worm stages
 #'
-#' The function takes in the channel data and sorts the worms into four different stages (L1, L2/3, L4, Adult)
+#' The function takes in the channel data and sorts the worms into six different stages (L1, L2,L3, L4,Young Adult, Adult)
 #' depending on their size.
 #'
 #' @param Channel Channel data
-#' @return A list with 4 stages of worms
+#' @return A list with 6 stages of worms
 #' @export
 AssignStage <- function(Channel) {
   if(missing(Channel)){
@@ -15,16 +15,20 @@ AssignStage <- function(Channel) {
   TOF <- matrix(,nrow=length(Channel[,1]),ncol = 2)
   for (x in 1:length(Channel[,1])) {
     TOF[x,1] <- which.min(Channel[x,])
-    if (as.numeric(TOF[x,1]) < 60 ) {TOF[x,2] <- "NA"} # I wasn't sure what to call things less than L1
-    else if (as.numeric(TOF[x,1]) >= 60 & as.numeric(TOF[x,1]) < 90 ) {TOF[x,2] <- "L1"}
-    else if (as.numeric(TOF[x,1]) >= 90 & as.numeric(TOF[x,1]) < 200 ) {TOF[x,2] <- "L2/L3"}
-    else if (as.numeric(TOF[x,1]) >= 200 & as.numeric(TOF[x,1]) < 300 ) {TOF[x,2] <- "L4"}
-    else if (as.numeric(TOF[x,1]) >= 300) {TOF[x,2] <- "Adult"}
+    if (as.numeric(TOF[x,1]) < 50 ) {TOF[x,2] <- "NA"} # I wasn't sure what to call things less than L1
+    else if (as.numeric(TOF[x,1]) >= 60 & as.numeric(TOF[x,1]) < 100 ) {TOF[x,2] <- "L1"}
+    else if (as.numeric(TOF[x,1]) >= 100 & as.numeric(TOF[x,1]) < 200 ) {TOF[x,2] <- "L2"}
+    else if (as.numeric(TOF[x,1]) >= 200 & as.numeric(TOF[x,1]) < 300 ) {TOF[x,2] <- "L2/L3"}
+    else if (as.numeric(TOF[x,1]) >= 300 & as.numeric(TOF[x,1]) < 600 ) {TOF[x,2] <- "L4"}
+    else if (as.numeric(TOF[x,1]) >= 600 & as.numeric(TOF[x,1]) < 900 ) {TOF[x,2] <- "YoungAdult"}
+    else if (as.numeric(TOF[x,1]) >= 900) {TOF[x,2] <- "Adult"}
   }
   StagesList <- list()
   StagesList[[1]] <- Channel[which(TOF[,2]=='L1'),]
-  StagesList[[2]] <- Channel[which(TOF[,2]=='L2/L3'),]
-  StagesList[[3]] <- Channel[which(TOF[,2]=='L4'),]
-  StagesList[[4]] <- Channel[which(TOF[,2]=='Adult'),]
+  StagesList[[2]] <- Channel[which(TOF[,2]=='L2'),]
+  StagesList[[3]] <- Channel[which(TOF[,2]=='L3'),]
+  StagesList[[4]] <- Channel[which(TOF[,2]=='L4'),]
+  StagesList[[5]] <- Channel[which(TOF[,2]=='YoungAdult'),]
+  StagesList[[6]] <- Channel[which(TOF[,2]=='Adult'),]
   return(StagesList)
 }
